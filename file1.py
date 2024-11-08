dc7b8224cb8fe57ee26ddb5562a0b430f8a955ad
prajwal <prajwalmr98@gmail.com>
2024-11-08 12:16:51 +0530

    removing of from darshan

--- file1.py
+++ file1.py
@@ -7,8 +7,8 @@ def sub(a,b):
 def main(a,b):
     res_add=add(a,b)
     res_sub = sub(a, b)
-    print(f"the addition of {a} and {b} is: {res_add}")
-    print(f"the substraction of {a} and {b} is: {res_sub}")
+    print(f"the addition  {a} and {b} is: {res_add}")
+    print(f"the substraction  {a} and {b} is: {res_sub}")
 
 
 main(2,4)
