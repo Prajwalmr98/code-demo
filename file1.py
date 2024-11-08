f625eb498c976992104f73282dac22e134209386
prajwal <prajwalmr98@gmail.com>
2024-11-08 11:31:34 +0530

    commiting sub function in darshan branch

--- file1.py
+++ file1.py
@@ -1,7 +1,11 @@
 def add(a,b):
     return a+b
+def sub(a,b):
+    return a-b
 
 def main(a,b):
     res_add=add(a,b)
-    print(f"the addition {a} and {b} is: {res_add}")
+    res_sub=sub(a,b)
+    print(f"the addition of {a} and {b} is: {res_add}")
+    print(f"the substraction of {a} and {b} is: {res_sub}")
 main(2,4)
