2d0f25a6bce6347c6e9aa4eae128d0d23717c618
prajwal <prajwalmr98@gmail.com>
2024-11-08 11:04:47 +0530

    commiting mul opertion prajwal branch

--- file1.py
+++ file1.py
@@ -1,7 +1,11 @@
 def add(a,b):
     return a+b
+def mul(a,b):
+    return a*b
 
 def main(a,b):
     res_add=add(a,b)
-    print(f"the addition {a} and {b} is: {res_add}")
+    res_mul=mul(a,b)
+    print(f"the addition of {a} and {b} is: {res_add}")
+    print(f"the multiplication of {a} and {b} is: {res_mul}")
 main(2,4)
