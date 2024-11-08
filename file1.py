814e23bdf058b9185c089fa6c269b44fdb03711d
prajwal <prajwalmr98@gmail.com>
2024-11-08 12:19:51 +0530

    Revert "removing of from prajwal"
    This reverts commit aa106aec83c2aab956dfddce8f751061988c6343.

--- file1.py
+++ file1.py
@@ -7,7 +7,7 @@ def main(a,b):
     res_add=add(a,b)
 
     res_mul=mul(a,b)
-    print(f"the addition  {a} and {b} is: {res_add}")
-    print(f"the multiplication  {a} and {b} is: {res_mul}")
+    print(f"the addition of {a} and {b} is: {res_add}")
+    print(f"the multiplication of {a} and {b} is: {res_mul}")
 
 main(2,4)
